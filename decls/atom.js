d42192f0f0fc3011f455d4410f79fc125054982b
Steel Brain <i@steelbrain.me>
2017-04-01 19:04:22 +0500

    :new: Implement working spawn and kill server

--- decls/atom.js
+++ decls/atom.js
@@ -1,3 +1,8 @@
 /* @flow */
 
-declare var atom: Object;
+declare var atom: any;
+declare module 'atom' {
+  declare var Disposable: any;
+  declare var BufferedProcess: any;
+  declare var CompositeDisposable: any;
+}
