7eb65f761931e011ec9e537127b0129dd69501a7
aminya <aminyahyaabadi74@gmail.com>
2019-11-11 19:12:37 -0600

    server run

--- script/StaticLintAtom/src/StaticLintAtom.jl
+++ script/StaticLintAtom/src/StaticLintAtom.jl
@@ -1,5 +1,16 @@
 module StaticLintAtom
 
-# Write your package code here.
+using LanguageServer
+using SymbolServer
+
+
+export server
+
+# named_pipe = ARGS[1] # julia.exe path
+named_pipe = "C:\\Users\\yahyaaba\\.julia\\environments\\v1.3"
+
+server = LanguageServerInstance(stdin, stdout, true, named_pipe, "", Dict())
+run(server)
+
 
 end
