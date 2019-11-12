5f73d2c9101814a315ebdffd6fbf5282188e05b8
aminya <aminyahyaabadi74@gmail.com>
2019-11-11 19:12:56 -0600

    calling StaticLintAtom from julia-server

--- script/StaticLint-juia-server.jl
+++ script/StaticLint-juia-server.jl
@@ -3,3 +3,7 @@ cd(@__DIR__)
 using Pkg;
 Pkg.activate(joinpath(pwd(),"StaticLintAtom"))
 Pkg.instantiate()
+
+using StaticLintAtom
+
+include(joinpath(pwd(),"StaticLintAtom","src","StaticLintAtom.jl"))
