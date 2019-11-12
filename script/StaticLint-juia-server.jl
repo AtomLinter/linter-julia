e367e3b3034af618d539951ad1816f6e4b7ce871
aminya <aminyahyaabadi74@gmail.com>
2019-11-11 19:14:41 -0600

    Running code inside the script itself

--- script/StaticLint-juia-server.jl
+++ script/StaticLint-juia-server.jl
@@ -4,6 +4,14 @@ using Pkg;
 Pkg.activate(joinpath(pwd(),"StaticLintAtom"))
 Pkg.instantiate()
 
-using StaticLintAtom
+# using StaticLintAtom
+# include(joinpath(pwd(),"StaticLintAtom","src","StaticLintAtom.jl"))
 
-include(joinpath(pwd(),"StaticLintAtom","src","StaticLintAtom.jl"))
+using LanguageServer
+using SymbolServer
+
+# named_pipe = ARGS[1] # julia.exe path
+named_pipe = "C:\\Users\\yahyaaba\\.julia\\environments\\v1.3"
+
+server = LanguageServerInstance(stdin, stdout, true, named_pipe, "", Dict())
+run(server)
--- script/StaticLintAtom/src/StaticLintAtom.jl
+++ script/StaticLintAtom/src/StaticLintAtom.jl
@@ -1,16 +1,3 @@
 module StaticLintAtom
 
-using LanguageServer
-using SymbolServer
-
-
-export server
-
-# named_pipe = ARGS[1] # julia.exe path
-named_pipe = "C:\\Users\\yahyaaba\\.julia\\environments\\v1.3"
-
-server = LanguageServerInstance(stdin, stdout, true, named_pipe, "", Dict())
-run(server)
-
-
 end
